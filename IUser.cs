ac64df3e5b81dce759573de921f809acecd64898
deepakjayakumar <deepakjayakumar11@gmail.com>
2023-07-19 12:50:35 +0530

    Add project files.

---
+++ IUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrincipleDemo
+{
+    internal interface IUser
+    {
+        bool Login(string username, string password);
+        bool Register (string username, string password,string email);
+        
+        
+    }
+    interface ILogger
+    {
+        void Logging(string message);
+    }
+
+    interface IEmail
+    {
+        bool Sendemail(string emailcontent);
+    }
+}
